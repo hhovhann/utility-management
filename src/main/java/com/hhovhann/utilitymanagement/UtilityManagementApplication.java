5fe78d7e71ed464082df3c14d3aecae1f4779928
hhovhann <haik.hovhanisyan@gmail.com>
2020-12-22 02:57:44 +0400

    Addedproject skeleton.

---
+++ src/main/java/com/hhovhann/utilitymanagement/UtilityManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hhovhann.utilitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilityManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhovhann/utilitymanagement/UtilityManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhovhann.utilitymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
